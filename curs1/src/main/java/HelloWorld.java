888320e248c298072004e4d128a7597885913446
RJ <jinga.robert@gmail.com>
2016-10-20 20:07:07 +0300

    Curs2

--- curs1/src/main/java/HelloWorld.java
+++ curs1/src/main/java/HelloWorld.java
@@ -1,23 +1,32 @@
-import ro.isr.deveschool.chess.Square;
+import ro.isr.devschool.chess.Square;
 
 import java.util.Collections;
 
 /**
  * Created by Mucefix on 16/10/16.
  */
+
 public class HelloWorld {
 
     static {
         /*
-          blocul acesta se va executa o singura data in momentul in care
-          clasa este incarcata de catre classloader
+        *blocul acesta se va executa o singura data in momentul in care clasa
+        *clasa este incarcata de catre classloader
          */
     }
 
     public static void main(String[] args) {
         System.out.printf("Hello World !");
 
-        Square square = new Square(2,3);
+        Square s = new Square(2,3);
+        s.getRow();
+        //System.out.println(s);
+
 
     }
 }
+
+// read about maven
+// get used to Intellij
+// checkout chess diagram from resource
+// oop diagram
